584bfdbb4a2fec000d0265aac17b9fb8b1514887
rodrigsneto <contact@rodrigsneto.com>
2024-01-19 15:39:20 -0300

    outra forma de verificar se é um array

--- src/inverte_lista.js
+++ src/inverte_lista.js
@@ -1,11 +1,7 @@
 function inverte_lista(array) {
-    if (Array.isArray(array) === false || array.length === 0) {
+    if (novo_array instanceof Array === false || array.length === 0) {
         return "Argumento inválido. Função espera receber um array como argumento.";
     }
 
     return array.reverse(array);
 }
-
-let novo_array = [
-    'arroz', 'feijao', 'bife', 'macarrao'
-]
