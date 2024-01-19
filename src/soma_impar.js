383060d93a44375eb39c3e616b86022b5b590ba4
rodrigsneto <contact@rodrigsneto.com>
2024-01-19 14:43:10 -0300

    correcao usando isNaN e parseInt

--- src/soma_impar.js
+++ src/soma_impar.js
@@ -1,17 +1,19 @@
 function soma_impar(x) {
-    if (isNaN(x) === false) {
-        return "Função aceita apenas números inteiros positivos.";
-    }
 
+    if (isNaN(x) === true) {
+        return "Função aceita apenas números inteiros positivos.";
+    } 
+    
+    
+    x = parseInt(x);
     let soma_final = 0;
     
-
     for(var i = 0; i < (x+1); i++) {
 
         if (i%2 != 0) {
             soma_final += i;
         }
-
+        
     }
 
     return soma_final;
