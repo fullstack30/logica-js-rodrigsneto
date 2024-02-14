5f09641aa15e856898ae90db515f7d3edcad4186
Rodrigues Neto <accounts@rodrigsneto.com>
2024-02-13 22:03:00 -0300

    Vogais (#19)
    * Resolvendo soma_impar
    
    * correcao usando isNaN e parseInt
    
    * validando numeros positivos ou negativos
    
    * validando float
    
    * solucao inverte_lista
    
    * outra forma de verificar se é um array
    
    * outra forma de verificar se é um array
    
    * outra forma de verificar se é um array
    
    * vogais 1.0
    
    * vogais 1.1
    
    * vogais 1.2

---
+++ src/vogais.js
@@ -0,0 +1,10 @@
+function vogais(params) {
+    if(typeof params == 'string') {
+        var string = params.split('');
+        var vogais = string.filter(caracter => /[aàáâãeéêèiíìoòõóôuúùûçAÀÁÂÃEÉÈÊIÍÌOÓÒÔÕUÇ]/.test(caracter));
+
+        return vogais.length;
+    }
+
+    return "Função aceita apenas string como argumento.";
+}
