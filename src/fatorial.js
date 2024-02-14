43ae769a2e2875ab84815907cf3ec16375ab9e53
rodrigsneto <contact@rodrigsneto.com>
2024-02-14 13:21:56 -0300

    fatorial 1.1

--- src/fatorial.js
+++ src/fatorial.js
@@ -1,5 +1,6 @@
 function fatorial(params) {
-    if (Number.isInteger(params) && params > 0) {
+    if (Number.isInteger(params) && params > 0 || (isNaN(params) == false)) {
+        params = parseInt(params);
 
         let fator = 1;
         for (i = 1; i < params + 1; i++ ) {
